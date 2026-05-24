ec3deb4e3360e11202da0730cb04f042ec84d8cc
joeltamayo <jueltamayo25@gmail.com>
2026-05-23 19:34:28 -0600

    Actualizacion para deploy

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 /**
  * URL base del backend Express para todos los endpoints del SPA.
  */
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://retos-fotograficos-api.onrender.com';
 
 /**
  * Hash de ruta usado para enviar al usuario al login cuando su sesión expira.
